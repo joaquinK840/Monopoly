6b473c2910a3a7cc98529a9ca85cd076dac89b05
joaquinK840 <joeshoca@gmail.com>
2025-09-05 08:59:25 -0500

    injec countries

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-fetch("http://127.0.0.1:5000/ranking")
+/* fetch("http://127.0.0.1:5000/ranking")
 .then((res) => res.json())
 .then((data) => {
     const scoreboard = document.getElementById("scoreboard");
@@ -11,5 +11,25 @@ fetch("http://127.0.0.1:5000/ranking")
     `;
     scoreboard.appendChild(div);
     });
-})
-.catch((err) => console.error("Error cargando datos:", err));
+}) 
+.catch((err) => console.error("Error cargando datos:", err)); */
+
+fetch("http://127.0.0.1:5000/countries")
+  .then((res) => res.json())
+  .then((data) => {
+    const scoreboard = document.getElementById("scoreboard");
+    data.forEach((country) => {
+      // obtener la clave y el valor del objeto
+      const [code, name] = Object.entries(country)[0];
+
+      const div = document.createElement("div");
+      div.classList.add("country");
+      div.innerHTML = `
+        <span>${name} (${code.toUpperCase()})</span>
+      `;
+      scoreboard.appendChild(div);
+    });
+  })
+  .catch((err) => console.error("Error cargando datos:", err));
+
+
