e79ab2f3d72caf4f35b67fdb99301e3fa6f95388
Sakuga-05 <sakuga.09@gmail.com>
2025-09-05 10:25:49 -0500

    Creación del login

--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -17,17 +17,17 @@
 fetch("http://127.0.0.1:5000/countries")
   .then((res) => res.json())
   .then((data) => {
-    const scoreboard = document.getElementById("scoreboard");
+    const select = document.getElementById("scoreboard");
+    select.innerHTML = '<option value="">-- Selecciona un país --</option>';
+
     data.forEach((country) => {
       // obtener la clave y el valor del objeto
       const [code, name] = Object.entries(country)[0];
 
-      const div = document.createElement("div");
-      div.classList.add("country");
-      div.innerHTML = `
-        <span>${name} (${code.toUpperCase()})</span>
-      `;
-      scoreboard.appendChild(div);
+      const option = document.createElement("option");
+      option.value = code;   // valor real (ej: 'us')
+      option.textContent = name; // texto visible (ej: 'United States')
+      select.appendChild(option);
     });
   })
   .catch((err) => console.error("Error cargando datos:", err));
