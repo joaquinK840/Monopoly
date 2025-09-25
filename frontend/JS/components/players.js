64e68543365fb40d1c303f1cd35b752477ae6451
Sakuga-05 <sakuga.09@gmail.com>
2025-09-25 18:16:56 -0500

    Cards de jugadores

---
+++ frontend/JS/components/players.js
@@ -0,0 +1,41 @@
+import { getPlayers } from "./login.js";
+
+export function renderPlayerContainers(containerId) {
+  let players = getPlayers();
+  
+  // Limpia el contenedor antes de renderizar
+  containerId.innerHTML = "";
+
+  // Generar un div por cada jugador
+  players.forEach(player => {
+    const div = document.createElement("div");
+    div.className = "player-container";
+    div.innerHTML = renderCardPlayer(player);
+    containerId.appendChild(div);
+  });
+}
+
+export function renderCardPlayer(player) {
+    return `
+    <div class="container py-3">
+        <div class="card shadow-sm" style="max-width: 200px;">
+            <div class="card-body">
+            <h5 class="card-title text-primary">${player.name}</h5>
+            
+            <div class="mb-2">
+                <span class="fw-bold">Dinero:</span> 0
+            </div>
+            
+            <div class="mb-3">
+                <label for="propiedades" class="form-label fw-bold">Propiedades:</label>
+                <select class="form-select" id="propiedades">
+                <option selected>propiedades</option>
+                </select>
+            </div>
+            
+            <button class="btn btn-primary w-100">Acción</button>
+            </div>
+        </div>
+    </div>
+    `
+}
