6b9160653d7a035ef97d6d6fbf4337350a61af81
joaquinK840 <joeshoca@gmail.com>
2025-09-25 14:36:29 -0500

    inyectar y renderizar tablero

--- frontend/JS/routes/board.js
+++ frontend/JS/routes/board.js
@@ -2,12 +2,17 @@ import { fetchBoard } from "../services/boardServices.js";
 import { renderBoard } from "../components/board.js";
 import { initDice } from "../components/dice.js";
 import { renderAllTokens } from "../components/motion.js";
+import { renderEmptyBoard,decorateBoard } from "../components/boardRender.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+
+   renderEmptyBoard();
+
   initDice(); // inicializar dados
 
   const boardData = await fetchBoard();
   renderBoard(boardData); // mostrar tablero
+  decorateBoard(boardData);
 
   renderAllTokens()
 });
