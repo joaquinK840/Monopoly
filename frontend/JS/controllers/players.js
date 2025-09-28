2e9c65a9ba5b32a7cc4c513100df286d93fceeb4
joaquinK840 <joeshoca@gmail.com>
2025-09-28 00:33:37 -0500

    fin de juego

--- frontend/JS/controllers/players.js
+++ frontend/JS/controllers/players.js
@@ -2,15 +2,7 @@ import { getPlayers } from "../components/login.js";
 
 
 // Función para mostrar los jugadores en el elemento con id "jugadores"
-export function showPlayers() {
-  const jugadoresElement = document.getElementById("jugadores");
-  if (jugadoresElement) {
-    const players = getPlayers();
-    jugadoresElement.textContent = Array.isArray(players)
-      ? players.map(p => p.name).join(", ")
-      : "";
-  }
-}
+
 
 // Función para obtener la cantidad de jugadores actuales
 export function getPlayersCount() {
